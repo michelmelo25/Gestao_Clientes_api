837202b481096a2e7d0968444300ca277a870c9d
Michel Melo <michelmelosj25@hotmail.com>
2022-01-29 18:17:12 -0300

    criacao de arquivo para inicio de projeto

---
+++ gestao_clientes_api/src/main/java/com/michel/gestao_clientes_api/GestaoClientesApiApplication.java
@@ -0,0 +1,13 @@
+package com.michel.gestao_clientes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoClientesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoClientesApiApplication.class, args);
+    }
+
+}
---
+++ gestao_clientes_api/src/test/java/com/michel/gestao_clientes_api/GestaoClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.gestao_clientes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoClientesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
